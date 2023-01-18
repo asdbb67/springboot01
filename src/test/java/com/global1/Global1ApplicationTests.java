1b4c1a309b7fc9c19e5e9f83537c350a61d585ec
asdbb67 <tkdwls0522@gmail.com>
2023-01-18 11:08:12 +0900

    [git - commit]
    index.html 위치를
    resource > templates 디렉토리로 이동
    
    1)
    thymeleaf fragment 적용
    fragments.html 의 <head> 에
    th:fragment="head" 설정하고
    
    2)
    index.html의 <head>에
    <head th:replace="fragments.html :: head"

---
+++ src/test/java/com/global1/Global1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.global1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Global1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
